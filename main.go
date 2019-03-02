496ce30174bcce66e4c1922e81985736968b1cbd
Atsushi KONISHI <crossover.ak@outlook.jp>
2019-03-02 22:34:35 +0900

    tweak

--- main.go
+++ main.go
@@ -46,18 +46,6 @@ func main() {
 			// (...)演算子は可変長引数に対し、可変長構造体を与える時につける
 
 			header := response.MakeResponseHeader(status, msg, getUTCTime(), loc)
-			// headerBuf := make([]byte, 0)
-			// fstLine := "HTTP/1.1 " + strconv.Itoa(status) + " " + msg + "\r\n"
-			// headerBuf = append(headerBuf, []byte(fstLine)...)
-			// // 2006年1月2日15時4分5秒 フォーマットの例文
-			// headerBuf = append(headerBuf, []byte("Date: "+ getUTCTime())...)
-			// // headerBuf = append(headerBuf, []byte("Date: "+ time.Now().Format("Tue, 30 Jul 2013 17:47:09 GMT\n"))...)
-			// headerBuf = append(headerBuf, []byte("Server: GolangServer\r\n")...)
-			// headerBuf = append(headerBuf, []byte("Connection: close\r\n")...)
-			// headerBuf = append(headerBuf, []byte("Content-Type: text/html\r\n")...)
-			// headerBuf = append(headerBuf, []byte("\r\n")...)
-			
-			// レスポンスボディを返す
 			conn.Write(append(header, buf...))
 			conn.Close()
 			// ヘッダとボディを分けないとブラウザに怒られる
